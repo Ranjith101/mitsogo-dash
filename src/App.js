82159246350f4136b9b62cbee9aecd645e960052
Ranjith101 <89287908+Ranjith101@users.noreply.github.com>
2023-02-07 18:07:40 +0530

    chart

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Sidebar from './Components/Sidebar';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Dashboard from './Components/Dashboard';
 
+
 function App() {
   return (
     <Router>
       <div>
         <Sidebar />
+
+        
       </div>
     </Router>
   );
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -7,8 +7,10 @@ import {
   CDBSidebarMenu,
   CDBSidebarMenuItem,
 } from 'cdbreact';
+import { CChart } from '@coreui/react-chartjs';
 import { NavLink } from 'react-router-dom';
 import Dashboard from './Dashboard';
+import Chart from './Chart';
 
 const Sidebar = () => {
   return (
@@ -41,7 +43,45 @@ const Sidebar = () => {
           </CDBSidebarMenu>
         </CDBSidebarContent>
       </CDBSidebar>
-      <Dashboard />
+      <CChart
+  type="line" 
+  data={{
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Stastics",
+        backgroundColor: "rgba(220, 220, 220, 0.2)",
+        borderColor: "rgba(220, 220, 220, 1)",
+        pointBackgroundColor: "rgba(220, 220, 220, 1)",
+        pointBorderColor: "#fff",
+        data: [40, 20, 12, 39, 10, 40, 39, 80, 40]
+      },
+      {
+        label: "My Second dataset",
+        backgroundColor: "rgba(151, 187, 205, 0.2)",
+        borderColor: "rgba(151, 187, 205, 1)",
+        pointBackgroundColor: "rgba(151, 187, 205, 1)",
+        pointBorderColor: "#fff",
+        data: [50, 12, 28, 29, 7, 25, 12, 70, 60]
+      },
+    ],
+  }}
+/>
+
+<CChart
+  type="doughnut"
+  data={{
+    labels: ['France', 'Italy', 'Japan', 'Canada'],
+    datasets: [
+      {
+        backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+        data: [4260, 3970, 4260, 3970],
+      },
+    ],
+  }}
+/>
+      {/* <Chart /> */}
+      {/* <Dashboard /> */}
     </div>
   );
 };
